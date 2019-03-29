90df34fb0d352d8a987e94187dcd98933badab1f
Jesper Asplund <jesper.asplund95@gmail.com>
2019-03-29 16:04:49 +0100

    Navbar added, no adjustments yet

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Navbar from './Navbar/index'
 // import Navbar from './Navbar/index'
 
 const Header = () => (
   <div className="header-wrapper">
     
-    <div style={{height: 100 + "px", width: 100 + "px", backgroundColor: "blue"}}>logo test</div>
+    <div className="logo-holder">
+      <Navbar />
+      <img src="/images/vojj-logo.png" className="img-fluid" alt="logo" />
+    </div>
   </div>
 )
 
