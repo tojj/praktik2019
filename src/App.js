c1e5c320f3b5577ef1aa2e206aa7deacd6148453
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-01 10:44:11 +0200

    added tags

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import StartPage from './components/StartPage/index'
-import Header from './components/Header/index'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import StartPage from "./components/StartPage/index";
+import Header from "./components/Header/index";
+import ProductInfo from "./components/StartPage/ProductInfo/index";
 
-const App = (props) => (
+const App = props => (
   <Router>
-    <header><Header /></header>
+    <header>
+      <Header />
+    </header>
     <main>
       <Switch>
         <Route exact path="/" component={StartPage} />
       </Switch>
+      <ProductInfo />
     </main>
     <footer>footer</footer>
   </Router>
-)
-export default App
+);
+export default App;
--- src/components/StartPage/ProductInfo/index.js
+++ src/components/StartPage/ProductInfo/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./style.scss";
 
-const ProductInfo = props => <div className="box-container" />;
-
+const ProductInfo = props => (
+  <div className="box-container">
+    <div className="box-1">
+      <h2>Bästa presenten</h2>
+      <p>
+        Med Vojj kan till och med Ken får det kalas han förtjänar. Klicka på
+        knappen nedan för att skapa ditt kalas nu!
+      </p>
+    </div>
+    <div className="box-2">
+      <img className="image" src="./prodinfobg.png" alt="Eye-catcher image" />
+    </div>
+  </div>
+);
 export default ProductInfo;
