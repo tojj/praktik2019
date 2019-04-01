db30c744a3ea293bd9f9171c72924cfb2b124e30
Pontus Nilsson <p0nta_@hotmail.com>
2019-04-01 13:44:29 +0200

    Fixed media queries and additional data change.

--- src/components/StartPage/about_us/index.js
+++ src/components/StartPage/about_us/index.js
@@ -5,11 +5,18 @@ import staticData from '../../../staticData'
 const About = (props) => (
   <div className="about-container" id="about-container">
     <h1 className="about-header">Om Oss</h1>
-    {staticData.map(renderInformationText)}
+    {staticData.aboutData.map(renderInformationText)}
     <div className="color-overlay" />
   </div>
 
 )
+
+/**
+ * 
+ * Skapade en subrenderkomponent nedanför som jag använder
+ * sedan till att mappa ut data från StaticData istället för att
+ * överanvända massa <p> så datan blir bloated.
+ */
 
 const renderInformationText = ({ text, id }) => {
 
