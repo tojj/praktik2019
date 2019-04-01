298c4bc13e77ab4bcbc55d0a38df854e3cbe214c
Pontus Nilsson <p0nta_@hotmail.com>
2019-04-01 11:56:38 +0200

    Added some minor styling.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Footer = (props) => (
-  <div className="footer-container">
+  <div className="footer-container" id="contact-us">
     <h1 className="footer-headliner">Vojj</h1>
+    
+
+    <div className="media-container">
+    </div>
   </div>
 )
 
