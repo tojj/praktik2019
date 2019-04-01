d1c921171a593d44e55f25b487a759875d0b01b9
Aleksandra <a.toholjevic@gmail.com>
2019-04-01 12:03:07 +0200

    test Eye Catcher

--- src/components/StartPage/EyeCatcher/index.js
+++ src/components/StartPage/EyeCatcher/index.js
@@ -2,14 +2,14 @@ import React from "react"
 import './styles.scss'
 
 
-const EyeCatcher = (props) => <div className="box-container">
+const EyeCatcher = (props) => <div className="box-container" id="eye-catcher">
   <div className="box-1">
-    <h2>Bästa presenten</h2>
-    <p>Med Vojj kan till och med Ken får det kalas han förtjänar. Klicka på knappen nedan för att skapa ditt kalas nu!</p>
+    <h2 className="main-heading" id="eye-catcher-heading">BÄSTA PRESENTEN</h2>
+    <p>Med Vojj kan till och med Kent får det kalas han förtjänar. Klicka på knappen nedan för att skapa ditt kalas nu!</p>
     <button className="link-eye-catcher">Skapa kalas</button>
   </div>
   <div className="box-2">
-    <img className="eye-catcher" src="/images/eye-catcher-img.png" alt="Eye-catcher image"></img></div>
+    <img className="eye-catcher" src="/images/eye-catcher-img.png" alt="Birthday card"></img></div>
 </div>
 
 export default EyeCatcher
