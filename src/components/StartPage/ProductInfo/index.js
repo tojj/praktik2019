16d4067417809a014c957903f0e0c4df02db0c4d
Pontus Nilsson <p0nta_@hotmail.com>
2019-04-02 14:30:29 +0200

    Fixed oliver component together with jesper.

--- src/components/StartPage/ProductInfo/index.js
+++ src/components/StartPage/ProductInfo/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import "./style.scss"
 
 const ProductInfo = props => (
   <div className="prodinfo-container">
@@ -8,19 +7,19 @@ const ProductInfo = props => (
     </div>
     <div className="prodinfo-box-2">
       <h2 className="prodinfo-header">Kvalitet över kvantitet</h2>
-      <p className=".info-text">
+      <p className="info-text">
         Barnkalas är jättekul, men varför ge bort flera presenter som ingen vill
         ha istället för att hålla det till några få riktigt bra presenter?
       </p>
-      <p className=".info-text">
+      <p className="info-text">
         Med Vojj är det nu enkelt att gå ihop och ge barnen något som de
         faktiskt önskar sig!
       </p>
-      <p className=".info-text">
+      <p className="info-text">
         Barnkalas är jättekul, men varför ge bort flera presenter som ingen vill
         ha istället för att hålla det till några få riktigt bra presenter?
       </p>
-      <p className=".info-text">
+      <p className="info-text">
         Med Vojj är det nu enkelt att gå ihop och ge barnen något som de
         faktiskt önskar sig!
       </p>
