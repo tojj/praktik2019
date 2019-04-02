36542b14657743808ea8378c8c1791397bad1574
Jesper Asplund <jesper.asplund95@gmail.com>
2019-04-02 15:03:08 +0200

    removed template folder + minor import change

--- src/components/StartPage/index.js
+++ src/components/StartPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import EyeCatcher from './EyeCatcher/index'
-import About from './about_us/index'
 import ProductInfo from './ProductInfo/index'
+import About from './about_us/index'
 
 const StartPage = () => (
   <div className="startpage-wrapper">
