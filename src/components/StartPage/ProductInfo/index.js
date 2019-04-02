865c3720e19c7c4e4a366cc31b6349dccc51f698
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-02 10:32:10 +0200

    almost done

--- src/components/StartPage/ProductInfo/index.js
+++ src/components/StartPage/ProductInfo/index.js
@@ -7,7 +7,15 @@ const ProductInfo = props => (
       <img className="fg-image" src="/images/prodinfobg.png" alt="fg image" />
     </div>
     <div className="box-2">
-      <h2 className="about-header">Kvalitet över kvantitet</h2>
+      <h2 className="prodinfo-header">Kvalitet över kvantitet</h2>
+      <p className=".info-text">
+        Barnkalas är jättekul, men varför ge bort flera presenter som ingen vill
+        ha istället för att hålla det till några få riktigt bra presenter?
+      </p>
+      <p className=".info-text">
+        Med Vojj är det nu enkelt att gå ihop och ge barnen något som de
+        faktiskt önskar sig!
+      </p>
       <p className=".info-text">
         Barnkalas är jättekul, men varför ge bort flera presenter som ingen vill
         ha istället för att hålla det till några få riktigt bra presenter?
