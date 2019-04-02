4d9d9e3295556c7edd2a08c244b405052e45fc37
Aleksandra <a.toholjevic@gmail.com>
2019-04-02 15:40:03 +0200

    mobile version convincer

---
+++ src/components/StartPage/Convincer/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Convincer = (props) => (
+  <div className="convincer-container">
+    <div className="convincer-box">
+      <div className="convincer-image-container">
+        <img className="convincer-image" src="/images/convincer4.jpg" alt="nåt" />
+      </div>
+      <div className="convincer-info">
+        <h4>Present</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit maximus nulla eget viverra. Nam porta id urna eget vestibulum.</p>
+      </div>
+    </div>
+    <div className="convincer-box">
+      <div className="convincer-image-container">
+        <img className="convincer-image" src="/images/convincer2.png" alt="nåt" />
+      </div>
+      <div className="convincer-info">
+        <h4>Bidra</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit maximus nulla eget viverra. Nam porta id urna eget vestibulum.</p>
+      </div>
+    </div>
+    <div className="convincer-box">
+      <div className="convincer-image-container">
+        <img className="convincer-image" src="/images/convincer3.jpg" alt="nåt" />
+      </div>
+      <div className="convincer-info">
+        <h4>Karma</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit maximus nulla eget viverra. Nam porta id urna eget vestibulum.</p>
+      </div>
+    </div>
+  </div>
+)
+
+
+export default Convincer
