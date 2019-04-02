286aa79600e941f8c20d6bde6cb973848595ee6d
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-02 13:37:53 +0200

    Kan vi bara köra med semicolon istället?

--- src/components/StartPage/index.js
+++ src/components/StartPage/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import About from "./about_us/index";
-import ProductInfo from "./ProductInfo/index";
+import React from "react"
+import About from "./about_us/index"
+import ProductInfo from "./ProductInfo/index"
 
 const StartPage = () => (
   <div className="startpage-wrapper">
     <ProductInfo />
     <About />
   </div>
-);
+)
 
-export default StartPage;
+export default StartPage
