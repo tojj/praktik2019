ce16796742c4fc3fa0f93e2c9b4f670d18434a29
Aleksandra <a.toholjevic@gmail.com>
2019-04-03 09:21:07 +0200

    minor changes in style and index.js

--- src/components/StartPage/Convincer/index.js
+++ src/components/StartPage/Convincer/index.js
@@ -4,27 +4,27 @@ const Convincer = (props) => (
   <div className="convincer-container">
     <div className="convincer-box">
       <div className="convincer-image-container">
-        <img className="convincer-image" src="/images/convincer4.jpg" alt="nåt" />
+        <img className="convincer-image" src="/images/convincer4.jpg" alt="Birthday present" />
       </div>
-      <div className="convincer-info">
+      <div className="convincer-info-container">
         <h4>Present</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit maximus nulla eget viverra. Nam porta id urna eget vestibulum.</p>
       </div>
     </div>
     <div className="convincer-box">
       <div className="convincer-image-container">
-        <img className="convincer-image" src="/images/convincer5.jpg" alt="nåt" />
+        <img className="convincer-image" src="/images/convincer5.jpg" alt="Swish" />
       </div>
-      <div className="convincer-info">
+      <div className="convincer-info-container">
         <h4>Bidra</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit maximus nulla eget viverra. Nam porta id urna eget vestibulum.</p>
       </div>
     </div>
     <div className="convincer-box">
       <div className="convincer-image-container">
-        <img className="convincer-image" src="/images/convincer3.jpg" alt="nåt" />
+        <img className="convincer-image" src="/images/convincer3.jpg" alt="Rädda barnen" />
       </div>
-      <div className="convincer-info">
+      <div className="convincer-info-container">
         <h4>Karma</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit maximus nulla eget viverra. Nam porta id urna eget vestibulum.</p>
       </div>
