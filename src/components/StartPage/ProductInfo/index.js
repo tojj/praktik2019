d92771a67982c6ea6f6663cf7fc5e0627575a75e
Jesper Asplund <jesper.asplund95@gmail.com>
2019-04-03 10:16:22 +0200

    100% done for the 3 components we have, last commit before mergin in the 4th

--- src/components/StartPage/ProductInfo/index.js
+++ src/components/StartPage/ProductInfo/index.js
@@ -3,18 +3,10 @@ import React from "react"
 const ProductInfo = props => (
   <div className="box-container prodinfo-container box-container">
     <div className="box prodinfo-box-1">
-      <img className="box-img fg-image" src="/images/prodinfobg.png" alt="fg" />
+      <img className="box-img fg-image" src="/images/eye-catcher-img.png" alt="fg" />
     </div>
     <div className="box prodinfo-box-2">
       <h2 className="prodinfo-header">Kvalitet över kvantitet</h2>
-      <p className="info-text">
-        Barnkalas är jättekul, men varför ge bort flera presenter som ingen vill
-        ha istället för att hålla det till några få riktigt bra presenter?
-      </p>
-      <p className="info-text">
-        Med Vojj är det nu enkelt att gå ihop och ge barnen något som de
-        faktiskt önskar sig!
-      </p>
       <p className="info-text">
         Barnkalas är jättekul, men varför ge bort flera presenter som ingen vill
         ha istället för att hålla det till några få riktigt bra presenter?
