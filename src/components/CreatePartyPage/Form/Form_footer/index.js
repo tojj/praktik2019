b24179e0b7663af5cc9131f955175870d5072558
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-04 16:01:00 +0200

    asfd

--- src/components/CreatePartyPage/Form/Form_footer/index.js
+++ src/components/CreatePartyPage/Form/Form_footer/index.js
@@ -2,11 +2,11 @@ import React from "react"
 import staticData from "../../../../staticData"
 import { Link } from "react-router-dom"
 
-const Form_header = props => (
+const Form_footer = props => (
   <div className="form-header-container">
     <div className="box-container">
       <div className="box" />
-      <Link to="/skapa-kalas" className="link-cancel">
+      <Link to="/" className="link-cancel">
         Avbryt
       </Link>
       <Link to="/skapa-kalas" className="link-party-page">
@@ -17,4 +17,4 @@ const Form_header = props => (
   </div>
 )
 
-export default Form_header
+export default Form_footer
