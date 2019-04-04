550b146cc7c504aeb9740e399173dcc02332567e
Aleksandra <a.toholjevic@gmail.com>
2019-04-04 12:00:16 +0200

    need to merge

--- src/components/CreatePartyPage/Form/Form_body/Event_input/index.js
+++ src/components/CreatePartyPage/Form/Form_body/Event_input/index.js
@@ -1,10 +1,35 @@
 import React from 'react'
 
 const EventInput = (props) => (
-  <div className="form-body-container">
-    hej events
+  <div className="box-container">
+    <div className="box">
+      <h2>Var, när & hur?</h2>
+      <img className="box-img fg-image" src="/images/time-place.png" alt="event" />
+    </div>
+    <div className="box">
+      <form>
+        <label style={textArea}>
+          <textarea >
+            Hello there, this is some text in a text area
+</textarea>
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+      </form>
+    </div>
+
   </div>
 )
 
+
+const textArea = {
+  display: "block"
+}
 export default EventInput
 
