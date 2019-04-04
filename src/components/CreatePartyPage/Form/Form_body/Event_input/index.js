1cf76aa92719f75bb3242bd32cfd56bbd7181bfa
Aleksandra <a.toholjevic@gmail.com>
2019-04-04 14:29:21 +0200

    almost done

--- src/components/CreatePartyPage/Form/Form_body/Event_input/index.js
+++ src/components/CreatePartyPage/Form/Form_body/Event_input/index.js
@@ -1,35 +1,56 @@
 import React from 'react'
 
-const EventInput = (props) => (
-  <div className="box-container">
-    <div className="box">
-      <h2>Var, när & hur?</h2>
-      <img className="box-img fg-image" src="/images/time-place.png" alt="event" />
-    </div>
-    <div className="box">
-      <form>
-        <label style={textArea}>
-          <textarea >
-            Hello there, this is some text in a text area
-</textarea>
-        </label>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-      </form>
-    </div>
+class EventInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Skriv gärna någonting om kalaset.."
+    }
+  }
 
-  </div>
-)
+  render() {
+    // let today = new Date
 
+    // today = new Date(today.setDate(today.getDate() - 1)).getTime()
 
-const textArea = {
-  display: "block"
+    return (
+
+      <div className="box-container">
+        <div className="box">
+          <h2 className="event-heading">Var, när <br /> &  hur?</h2>
+          <img className="box-img fg-image" src="/images/time-place.png" alt="event" />
+        </div>
+        <div className="box">
+          <form>
+            <label className="birthday-label">
+              <span className="input-heading"> Skriv några ord till de inbjudna:</span>
+              <textarea className="textarea-label form-input input100" value={this.state.value} />
+            </label>
+            <label className="birthday-label">
+              <span className="input-heading"> När är kalaset?</span>
+              <div className="time-label">
+                <input className="form-input input25" rows="2" type="text" placeholder="2019-04-13" />
+                <input className="form-input input25" rows="2" type="text" placeholder="14:00" />
+              </div>
+            </label>
+            <label className="birthday-label">
+              <span className="input-heading">Plats:</span>
+              <input className="form-input input100" rows="2" type="text" placeholder="Skattkammargatan 7" />
+              <div className="time-label">
+                <input className="form-input input25" rows="2" type="text" placeholder="127 44" />
+                <input className="form-input input25" rows="2" type="text" placeholder="Trollhättan" />
+              </div>
+            </label>
+            <label className="birthday-label">
+              <span className="input-heading">När vill du senast ha svar om vem som kommer?</span>
+              <input className="form-input input100" rows="2" type="date" />
+            </label>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
+
 export default EventInput
 
