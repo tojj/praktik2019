5661b2d327ce8020ecd12aa595cd09887c2a3a70
Jesper Asplund <jesper.asplund95@gmail.com>
2019-04-05 16:03:20 +0200

    Added router switch to send props to NavBar for content change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,13 @@ import Navbar from './Navbar/index'
 import ScrollToTop from 'react-scroll-up'
 import { NavLink } from 'react-router-dom'
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className="header-wrapper" id="header">
       <ScrollToTop style={{zIndex: 99}} showUnder={500} >
         <img className="arrow-img" src="/images/arrow.png" alt="back to top" />
       </ScrollToTop>
-      <Navbar />
+      <Navbar showFull={props.startPage} />
       <NavLink className="logo-holder" to="/">
         <img src="/images/vojj-logo.png" className="logo-img" alt="logo" />
       </NavLink>
