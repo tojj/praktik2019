a62fcbb1d693332f57e9c5d2af4d02e5297a86a1
Jesper Asplund <jesper.asplund95@gmail.com>
2019-04-05 14:55:12 +0200

    removed extra wrapping div in nav

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -17,47 +17,45 @@ class Navbar extends React.Component {
   }
   render() {
     return (
-      <div>
-        <nav>
-          <div className="burger-menu-btn" onClick={this.toggle}>
-            <img src="/images/burger-primary.png" alt="open menu" />
+      <nav>
+        <div className="burger-menu-btn" onClick={this.toggle}>
+          <img src="/images/burger-primary.png" alt="open menu" />
+        </div>
+        <div className={this.state.burgerOpen ? 'nav-wrapper open' : 'nav-wrapper'}>
+          <div>
+            <img className="burger-menu-btn" onClick={this.toggle} alt="close menu" src="/images/burger-secondary.png" />
           </div>
-          <div className={this.state.burgerOpen ? 'nav-wrapper open' : 'nav-wrapper'}>
-            <div>
-              <img className="burger-menu-btn" onClick={this.toggle} alt="close menu" src="/images/burger-secondary.png" />
-            </div>
-            <Link
-              spy={true}
-              smooth={true}
-              duration={500}
-              to="prodinfo-container"
-              className="link-item">tojj
-            </Link>
-            <Link
-              spy={true}
-              smooth={true}
-              duration={500}
-              to="convincer-container"
-              className="link-item">Varför?
-            </Link>
-            <Link
-              spy={true}
-              smooth={true}
-              duration={500}
-              role="link"
-              to="about-container"
-              className="link-item">Om oss
-            </Link>
-            <Link
-              spy={true}
-              smooth={true}
-              duration={500}
-              to="contact-us"
-              className="link-item">Kontakt
-            </Link>
-          </div>
-        </nav>
-      </div>
+          <Link
+            spy={true}
+            smooth={true}
+            duration={500}
+            to="prodinfo-container"
+            className="link-item">tojj
+          </Link>
+          <Link
+            spy={true}
+            smooth={true}
+            duration={500}
+            to="convincer-container"
+            className="link-item">Varför?
+          </Link>
+          <Link
+            spy={true}
+            smooth={true}
+            duration={500}
+            role="link"
+            to="about-container"
+            className="link-item">Om oss
+          </Link>
+          <Link
+            spy={true}
+            smooth={true}
+            duration={500}
+            to="contact-us"
+            className="link-item">Kontakt
+          </Link>
+        </div>
+      </nav>
     )
   }
 }
