b93060378b28a9725a74566a68cec067409b9347
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-05 16:05:14 +0200

    added sliders for charity selection

--- src/components/CreatePartyPage/Form/Form_footer/index.js
+++ src/components/CreatePartyPage/Form/Form_footer/index.js
@@ -1,20 +1,70 @@
 import React from "react"
 import staticData from "../../../../staticData"
 import { Link } from "react-router-dom"
+import Slider from "react-slick"
 
 const Form_footer = props => (
-  <div className="form-header-container">
+  <div className="form-footer-container">
     <div className="box-container">
-      <div className="box" />
+      <div className="box">
+        <label className="birthday-label">
+          Telefonnummer för swish
+          <input className="birthday-headline input100" rows="2" type="text" />
+        </label>
+      </div>
+      <div className="box align-left">
+        <label className="birthday-label">
+          Vad vill du göra med överskott?{" "}
+        </label>
+        <div className=".align-left">
+          <input className="inputauto align-left" type="checkbox" />
+          <label className="align-left">Skänk till välgörenhet</label>
+          <input className="inputauto align-left " type="checkbox" />
+          <label className="align-left">Mer påkostat kalas</label>
+        </div>
+      </div>
+    </div>
+    <div className="slider-container">
+      <div className="slider-content">
+        <Slider {...settings}>
+          <div className="testdiv">
+            <img className="charImg" src="/images/barncancer-fonden.jpg" />
+          </div>
+          <div className="testdiv">
+            <img className="charImg" src="/images/radda-barnen.png" />
+          </div>
+          <div className="testdiv">
+            <img className="charImg" src="/images/bris-ny.jpg" />
+          </div>
+          <div className="testdiv">
+            <img className="charImg" src="/images/sos-ny.jpg" />
+          </div>
+          <div className="testdiv">
+            <img className="charImg" src="/images/redcross.jpg" />
+          </div>
+          <div className="testdiv">
+            <img className="charImg" src="/images/habitat.jpg" />
+          </div>
+        </Slider>
+      </div>
+    </div>
+    <div className="buttons-container">
       <Link to="/" className="link-cancel">
         Avbryt
       </Link>
       <Link to="/skapa-kalas" className="link-party-page">
         Godkänn
       </Link>
-      <div className="box" />
     </div>
   </div>
 )
+
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 400,
+  slidesToShow: 4,
+  slidesToScroll: 1
+}
 
 export default Form_footer
--- src/components/CreatePartyPage/Form/Form_header/index.js
+++ src/components/CreatePartyPage/Form/Form_header/index.js
@@ -1,33 +1,31 @@
-import React from 'react'
-import staticData from '../../../../staticData'
+import React from "react"
+import staticData from "../../../../staticData"
 
-const Form_header = (props) => (
+const Form_header = props => (
   <div className="form-header-container">
     <div className="box-container">
       <div className="box">
         <h2 className="form-headline">Skapa Kalas</h2>
-        <form>
-          {(staticData.formHeaderData.map(renderLabels))}
-        </form>
+        <form>{staticData.formHeaderData.map(renderLabels)}</form>
       </div>
 
-      <div className="box">
-
-      </div>
+      <div className="box" />
     </div>
   </div>
 )
 
 const renderLabels = ({ className, defaultValue, id, text }) => {
-
   return (
     <label className="birthday-label" key={id}>
       {text}
-      <input className={className} rows="2" type="text" defaultValue={defaultValue} />
+      <input
+        className={className}
+        rows="2"
+        type="text"
+        placeholder={defaultValue}
+      />
     </label>
   )
 }
 
-
 export default Form_header
-
