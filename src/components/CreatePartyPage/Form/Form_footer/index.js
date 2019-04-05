aeefe8d00b8bf2d2421da56b1fc7dd3dda35aa71
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-05 16:51:21 +0200

    made sliders responsive, still needs work on breakpoints

--- src/components/CreatePartyPage/Form/Form_footer/index.js
+++ src/components/CreatePartyPage/Form/Form_footer/index.js
@@ -65,7 +65,33 @@ const settings = {
   infinite: true,
   speed: 400,
   slidesToShow: 4,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 }
 
 export default Form_footer
