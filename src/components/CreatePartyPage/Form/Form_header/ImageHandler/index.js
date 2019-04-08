f6cd6eca95c5dceb0561d594f56d7e8c0345e45e
Pontus Nilsson <p0nta_@hotmail.com>
2019-04-08 10:33:31 +0200

    Deleted image handler putting it as a new task.

--- src/components/CreatePartyPage/Form/Form_header/ImageHandler/index.js
+++ src/components/CreatePartyPage/Form/Form_header/ImageHandler/index.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import { Image } from 'react-feather'
+/* import { Image } from 'react-feather' */
 
 const ImageHandler = (props) => (
   <div className="handler-container">
-    <div className="image-container">
-      <p className="add-text">Lägg till bild</p>
-      <Image className="image-icon" />
-    </div>
-      <button className="image-dropdown"><Image />Våra urval</button>
   </div>
 )
 
