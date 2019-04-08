f474fcc598da77c89bf264cf8421dab2c72627be
Jesper Asplund <jesper.asplund95@gmail.com>
2019-04-08 15:00:58 +0200

    Minor update to event_input

--- src/components/CreatePartyPage/Form/Form_body/Event_input/index.js
+++ src/components/CreatePartyPage/Form/Form_body/Event_input/index.js
@@ -4,33 +4,33 @@ const EventInput = (props) => (
 
   <div className="box-container">
     <div className="box">
-      <h2 className="event-heading">Var, när <br /> &  hur?</h2>
+      <h2 className="event-heading">Var, när <br /> &amp; hur?</h2>
       <img className="box-img fg-image" src="/images/time-place.png" alt="event" />
     </div>
     <div className="box">
       <form>
         <label className="birthday-label">
-          <span className="input-heading"> Skriv några ord till de inbjudna:</span>
+          <span className="input-heading">Skriv några ord till de inbjudna:</span>
           <textarea className="textarea-label form-input input100" value="Skriv gärna någonting om kalaset..." />
         </label>
         <label className="birthday-label">
-          <span className="input-heading"> När är kalaset?</span>
+          <span className="input-heading">När är kalaset?</span>
           <div className="time-label">
-            <input className="form-input input25" rows="2" type="text" placeholder="2019-04-13" />
-            <input className="form-input input25" rows="2" type="text" placeholder="14:00" />
+            <input className="form-input input50" rows="2" type="date" />
+            <input className="form-input input25" rows="2" type="time" value="12:00" />
           </div>
         </label>
         <label className="birthday-label">
           <span className="input-heading">Plats:</span>
-          <input className="form-input input100" rows="2" type="text" placeholder="Skattkammargatan 7" />
+          <input className="form-input input100" rows="2" type="text" placeholder="Gatuadress" />
           <div className="time-label">
-            <input className="form-input input25" rows="2" type="text" placeholder="127 44" />
-            <input className="form-input input25" rows="2" type="text" placeholder="Trollhättan" />
+            <input className="form-input input25" rows="2" type="text" placeholder="Postnummer" />
+            <input className="form-input input25" rows="2" type="text" placeholder="Stad" />
           </div>
         </label>
         <label className="birthday-label">
           <span className="input-heading">När vill du senast ha svar om vem som kommer?</span>
-          <input className="form-input input100" rows="2" type="date" />
+          <input className="form-input input50" rows="2" type="date" />
         </label>
       </form>
     </div>
