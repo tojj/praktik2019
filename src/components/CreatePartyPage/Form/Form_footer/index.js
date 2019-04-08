d1b0d0c7366f3e8adb8df7f31925ee09d74be598
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-08 12:15:19 +0200

    fixed breakpoints

--- src/components/CreatePartyPage/Form/Form_footer/index.js
+++ src/components/CreatePartyPage/Form/Form_footer/index.js
@@ -70,7 +70,7 @@ const settings = {
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1170,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -79,7 +79,7 @@ const settings = {
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 875,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
@@ -87,7 +87,7 @@ const settings = {
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 595,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
