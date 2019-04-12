09166cf38a220bf5dae1b3540fdd90f23a5f453b
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-12 13:58:17 +0200

    converted to class and added click event

--- src/components/CreatePartyPage/Form/Web_shop/index.js
+++ src/components/CreatePartyPage/Form/Web_shop/index.js
@@ -1,56 +1,79 @@
 import React, { Component } from "react"
 
-const Web_shop = props => (
-  <div className="webshop-container">
-    <h2 className="form-headline charity-headline text-center">Välj present</h2>
-    <div className="shop-item-container">
-      <div className="shop-item">
-        <img className="shop-img" src="/images/elefant.png" alt="event" />
-        <div className="shop-info">
-          <p>Kay Bojesen Flodhäst Träleksak</p>
-          <p>Pris: 1000kr</p>
-        </div>
-      </div>
-      <div className="shop-item">
-        <div className="shop-item-overlay"> </div>
-        <div className="shop-item-checkmark"> </div>
-        <img className="shop-img" src="/images/elefant.png" alt="event" />
-        <div className="shop-info">
-          <p>Kay Bojesen Flodhäst Träleksak</p>
-          <p>Pris: 1000kr</p>
-        </div>
-      </div>
-      <div className="shop-item">
-        <img className="shop-img" src="/images/elefant.png" alt="event" />
-        <div className="shop-info">
-          <p>Kay Bojesen Flodhäst Träleksak</p>
-          <p>Pris: 1000kr</p>
-        </div>
-      </div>
+class Web_shop extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false
+    }
+    this.toggleClass = this.toggleClass.bind(this)
+  }
 
-      <div className="shop-item">
-        <img className="shop-img" src="/images/elefant.png" alt="event" />
-        <div className="shop-info">
-          <p>Kay Bojesen Flodhäst Träleksak</p>
-          <p>Pris: 1000kr</p>
+  toggleClass() {
+    const currentState = this.state.active
+    this.setState({ active: !currentState })
+  }
+
+  render() {
+    return (
+      <div className="webshop-container">
+        <h2 className="form-headline charity-headline text-center">
+          Välj present
+        </h2>
+        <div className="shop-item-container">
+          <div className="shop-item" onClick={this.toggleClass}>
+            <div className={this.state.active ? "shop-item-overlay" : ""} />
+            <div className={this.state.active ? "shop-item-checkmark" : ""} />
+            <img className="shop-img" src="/images/elefant.png" alt="event" />
+            <div className="shop-info">
+              <p>Kay Bojesen Flodhäst Träleksak</p>
+              <p>Pris: 1000kr</p>
+            </div>
+          </div>
+          <div className="shop-item" onClick={this.toggleClass}>
+            <div className={this.state.active ? "shop-item-overlay" : ""} />
+            <div className={this.state.active ? "shop-item-checkmark" : ""} />
+            <img className="shop-img" src="/images/elefant.png" alt="event" />
+            <div className="shop-info">
+              <p>Kay Bojesen Flodhäst Träleksak</p>
+              <p>Pris: 1000kr</p>
+            </div>
+          </div>
+          <div className="shop-item" onClick={this.toggleClass}>
+            <div className={this.state.active ? "shop-item-overlay" : ""} />
+            <div className={this.state.active ? "shop-item-checkmark" : ""} />
+            <img className="shop-img" src="/images/elefant.png" alt="event" />
+            <div className="shop-info">
+              <p>Kay Bojesen Flodhäst Träleksak</p>
+              <p>Pris: 1000kr</p>
+            </div>
+          </div>
+
+          <div className="shop-item">
+            <img className="shop-img" src="/images/elefant.png" alt="event" />
+            <div className="shop-info">
+              <p>Kay Bojesen Flodhäst Träleksak</p>
+              <p>Pris: 1000kr</p>
+            </div>
+          </div>
+          <div className="shop-item">
+            <img className="shop-img" src="/images/elefant.png" alt="event" />
+            <div className="shop-info">
+              <p>Kay Bojesen Flodhäst Träleksak</p>
+              <p>Pris: 1000kr</p>
+            </div>
+          </div>
+          <div className="shop-item">
+            <img className="shop-img" src="/images/elefant.png" alt="event" />
+            <div className="shop-info">
+              <p>Kay Bojesen Flodhäst Träleksak</p>
+              <p>Pris: 1000kr</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="shop-item">
-        <img className="shop-img" src="/images/elefant.png" alt="event" />
-        <div className="shop-info">
-          <p>Kay Bojesen Flodhäst Träleksak</p>
-          <p>Pris: 1000kr</p>
-        </div>
-      </div>
-      <div className="shop-item">
-        <img className="shop-img" src="/images/elefant.png" alt="event" />
-        <div className="shop-info">
-          <p>Kay Bojesen Flodhäst Träleksak</p>
-          <p>Pris: 1000kr</p>
-        </div>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Web_shop
