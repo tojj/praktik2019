eed6616156bad950251bafb52044a70cb17a6d90
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-12 09:41:28 +0200

    added images and text to store

--- src/components/CreatePartyPage/Form/Web_shop/index.js
+++ src/components/CreatePartyPage/Form/Web_shop/index.js
@@ -1,5 +1,54 @@
 import React, { Component } from "react"
 
-const Web_shop = props => <div />
+const Web_shop = props => (
+  <div className="webshop-container">
+    <h2 className="form-headline charity-headline text-center">Välj present</h2>
+    <div className="shop-item-container">
+      <div className="shop-item">
+        <img className="shop-img" src="/images/elefant.png" alt="event" />
+        <div className="shop-info">
+          <p>Kay Bojesen Flodhäst Träleksak</p>
+          <p>Pris: 1000kr</p>
+        </div>
+      </div>
+      <div className="shop-item">
+        <img className="shop-img" src="/images/elefant.png" alt="event" />
+        <div className="shop-info">
+          <p>Kay Bojesen Flodhäst Träleksak</p>
+          <p>Pris: 1000kr</p>
+        </div>
+      </div>
+      <div className="shop-item">
+        <img className="shop-img" src="/images/elefant.png" alt="event" />
+        <div className="shop-info">
+          <p>Kay Bojesen Flodhäst Träleksak</p>
+          <p>Pris: 1000kr</p>
+        </div>
+      </div>
+
+      <div className="shop-item">
+        <img className="shop-img" src="/images/elefant.png" alt="event" />
+        <div className="shop-info">
+          <p>Kay Bojesen Flodhäst Träleksak</p>
+          <p>Pris: 1000kr</p>
+        </div>
+      </div>
+      <div className="shop-item">
+        <img className="shop-img" src="/images/elefant.png" alt="event" />
+        <div className="shop-info">
+          <p>Kay Bojesen Flodhäst Träleksak</p>
+          <p>Pris: 1000kr</p>
+        </div>
+      </div>
+      <div className="shop-item">
+        <img className="shop-img" src="/images/elefant.png" alt="event" />
+        <div className="shop-info">
+          <p>Kay Bojesen Flodhäst Träleksak</p>
+          <p>Pris: 1000kr</p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
 
 export default Web_shop
