65206a27e0c0d590634c5002505f1a11dc1d3538
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-15 11:06:59 +0200

    terrible but it works

--- src/components/CreatePartyPage/Form/Web_shop/index.js
+++ src/components/CreatePartyPage/Form/Web_shop/index.js
@@ -4,14 +4,26 @@ class Web_shop extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      active: false
+      active1: 0,
+      active2: 0,
+      active3: 0,
+      active4: 0,
+      active5: 0,
+      active6: 0
     }
     this.toggleClass = this.toggleClass.bind(this)
   }
 
-  toggleClass() {
-    const currentState = this.state.active
-    this.setState({ active: !currentState })
+  toggleClass(i) {
+    for (let i = 1; i < 7; i++) {
+      let loopIndex = "active" + i
+      this.setState({ [loopIndex]: 0 })
+    }
+    if (this.state[i] === 0) {
+      this.setState({ [i]: 1 })
+    } else {
+      this.setState({ [i]: 0 })
+    }
   }
 
   render() {
@@ -21,27 +33,52 @@ class Web_shop extends React.Component {
           Välj present
         </h2>
         <div className="shop-item-container">
-          <div className="shop-item" onClick={this.toggleClass}>
-            <div className={this.state.active ? "shop-item-overlay" : ""} />
-            <div className={this.state.active ? "shop-item-checkmark" : ""} />
+          <div
+            className="shop-item"
+            id="1"
+            onClick={e => this.toggleClass("active1", e)}
+          >
+            <div className={this.state.active1 ? "shop-item-overlay" : ""} />
+            <div className={this.state.active1 ? "shop-item-checkmark" : ""} />
             <img className="shop-img" src="/images/elefant.png" alt="event" />
             <div className="shop-info">
               <p>Kay Bojesen Flodhäst Träleksak</p>
               <p>Pris: 1000kr</p>
             </div>
           </div>
-          <div className="shop-item" onClick={this.toggleClass}>
-            <div className={this.state.active ? "shop-item-overlay" : ""} />
-            <div className={this.state.active ? "shop-item-checkmark" : ""} />
+          <div
+            className="shop-item"
+            id="1"
+            onClick={e => this.toggleClass("active2", e)}
+          >
+            <div className={this.state.active2 ? "shop-item-overlay" : ""} />
+            <div className={this.state.active2 ? "shop-item-checkmark" : ""} />
             <img className="shop-img" src="/images/elefant.png" alt="event" />
             <div className="shop-info">
               <p>Kay Bojesen Flodhäst Träleksak</p>
               <p>Pris: 1000kr</p>
             </div>
           </div>
-          <div className="shop-item" onClick={this.toggleClass}>
-            <div className={this.state.active ? "shop-item-overlay" : ""} />
-            <div className={this.state.active ? "shop-item-checkmark" : ""} />
+          <div
+            className="shop-item"
+            id="1"
+            onClick={e => this.toggleClass("active3", e)}
+          >
+            <div className={this.state.active3 ? "shop-item-overlay" : ""} />
+            <div className={this.state.active3 ? "shop-item-checkmark" : ""} />
+            <img className="shop-img" src="/images/elefant.png" alt="event" />
+            <div className="shop-info">
+              <p>Kay Bojesen Flodhäst Träleksak</p>
+              <p>Pris: 1000kr</p>
+            </div>
+          </div>
+          <div
+            className="shop-item"
+            id="1"
+            onClick={e => this.toggleClass("active4", e)}
+          >
+            <div className={this.state.active4 ? "shop-item-overlay" : ""} />
+            <div className={this.state.active4 ? "shop-item-checkmark" : ""} />
             <img className="shop-img" src="/images/elefant.png" alt="event" />
             <div className="shop-info">
               <p>Kay Bojesen Flodhäst Träleksak</p>
@@ -49,21 +86,26 @@ class Web_shop extends React.Component {
             </div>
           </div>
 
-          <div className="shop-item">
+          <div
+            className="shop-item"
+            id="1"
+            onClick={e => this.toggleClass("active5", e)}
+          >
+            <div className={this.state.active5 ? "shop-item-overlay" : ""} />
+            <div className={this.state.active5 ? "shop-item-checkmark" : ""} />
             <img className="shop-img" src="/images/elefant.png" alt="event" />
             <div className="shop-info">
               <p>Kay Bojesen Flodhäst Träleksak</p>
               <p>Pris: 1000kr</p>
             </div>
           </div>
-          <div className="shop-item">
-            <img className="shop-img" src="/images/elefant.png" alt="event" />
-            <div className="shop-info">
-              <p>Kay Bojesen Flodhäst Träleksak</p>
-              <p>Pris: 1000kr</p>
-            </div>
-          </div>
-          <div className="shop-item">
+          <div
+            className="shop-item"
+            id="1"
+            onClick={e => this.toggleClass("active6", e)}
+          >
+            <div className={this.state.active6 ? "shop-item-overlay" : ""} />
+            <div className={this.state.active6 ? "shop-item-checkmark" : ""} />
             <img className="shop-img" src="/images/elefant.png" alt="event" />
             <div className="shop-info">
               <p>Kay Bojesen Flodhäst Träleksak</p>
