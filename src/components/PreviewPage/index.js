c49f1945f7be31855dfd1f76e1504967a6b589a9
Pontus Nilsson <p0nta_@hotmail.com>
2019-04-15 10:15:59 +0200

    Added minor SASS styling

--- src/components/PreviewPage/index.js
+++ src/components/PreviewPage/index.js
@@ -11,25 +11,27 @@ class PreviewPage extends Component {
     console.log(this.props)
 
     return (
-      <div className="preview-container">
-        <h1 className="birthday-kid"> {this.props.birthdayEvent.title}</h1>
-        <div className="information-wrapper">
-          <div className="desc-state">
-            <h2>{this.props.birthdayEvent.name} FYLLER {this.props.birthdayEvent.age} ÅR</h2>
-            <p>Desc State</p>
+      <div className="background-wrapper">
+        <div className="preview-container">
+          <h1 className="birthday-kid"> {this.props.birthdayEvent.title}</h1>
+          <div className="information-wrapper">
+            <div className="desc-state">
+              <h2>{this.props.birthdayEvent.name} FYLLER {this.props.birthdayEvent.age} ÅR</h2>
+              <p>Desc State</p>
+            </div>
+            <div className="date-state">Datum states</div>
           </div>
-          <div className="date-state">Datum states</div>
-        </div>
 
-        <div className="swish-wrapper">
-          <div className="swish-child">QR Logo</div>
-          <div className="swish-child">Swish State Info</div>
-          <div className="swish-child">Karma</div>
-        </div>
+          <div className="swish-wrapper">
+            <div className="swish-child">QR Logo</div>
+            <div className="swish-child">Swish State Info</div>
+            <div className="swish-child">Karma</div>
+          </div>
 
-        <div className="map-wrapper">
-          <div className="map-child">Google maps</div>
-          <div className="map-child">Adress och OSA States</div>
+          <div className="map-wrapper">
+            <div className="map-child">Google maps</div>
+            <div className="map-child">Adress och OSA States</div>
+          </div>
         </div>
       </div>
     )
