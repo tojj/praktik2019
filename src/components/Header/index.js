60db8f6cc9f17ea4c05b64bc6b4ce345b820b169
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-16 11:30:17 +0200

    Added new logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Navbar from './Navbar/index'
-import ScrollToTop from 'react-scroll-up'
-import { NavLink } from 'react-router-dom'
+import React from "react"
+import Navbar from "./Navbar/index"
+import ScrollToTop from "react-scroll-up"
+import { NavLink } from "react-router-dom"
 
-const Header = (props) => {
+const Header = props => {
   return (
     <div className="header-wrapper" id="header">
-      <ScrollToTop style={{zIndex: 99}} showUnder={500} >
+      <ScrollToTop style={{ zIndex: 99 }} showUnder={500}>
         <img className="arrow-img" src="/images/arrow.png" alt="back to top" />
       </ScrollToTop>
       <Navbar showFull={props.startPage} />
       <NavLink className="logo-holder" to="/">
-        <img src="/images/tojj-logo.png" className="logo-img" alt="logo" />
+        <img src="/images/tojj-logo2.png" className="logo-img" alt="logo" />
       </NavLink>
     </div>
   )
