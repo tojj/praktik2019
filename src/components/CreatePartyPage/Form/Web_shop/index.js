9bee8de86a652d29b8df7799c00410b697144bdb
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-16 10:36:09 +0200

    individually clickable infopages

--- src/components/CreatePartyPage/Form/Web_shop/index.js
+++ src/components/CreatePartyPage/Form/Web_shop/index.js
@@ -10,7 +10,12 @@ class Web_shop extends React.Component {
       active4: 0,
       active5: 0,
       active6: 0,
-      presentInfo: false
+      presentInfo1: false,
+      presentInfo2: false,
+      presentInfo3: false,
+      presentInfo4: false,
+      presentInfo5: false,
+      presentInfo6: false
     }
     this.toggleClass = this.toggleClass.bind(this)
     this.toggleInfo = this.toggleInfo.bind(this)
@@ -28,9 +33,9 @@ class Web_shop extends React.Component {
     }
   }
 
-  toggleInfo() {
-    const currVal = this.state.presentInfo
-    this.setState({ presentInfo: !currVal })
+  toggleInfo(i) {
+    const currVal = this.state[i]
+    this.setState({ [i]: !currVal })
   }
 
   render() {
@@ -43,68 +48,276 @@ class Web_shop extends React.Component {
           <div
             className={this.state.active1 ? "shop-item-border" : "shop-item"}
             id="1"
-            onClick={e => this.toggleClass("active1", e)}
           >
             <div className={this.state.active1 ? "shop-item-overlay" : ""} />
-            <div className={this.state.active1 ? "shop-item-checkmark" : ""} />
-            <img className="shop-img" src="/images/elefant.png" alt="event" />
-            <div className="shop-info">
-              <p>Kay Bojesen Flodhäst Träleksak</p>
-              <p>Pris: 1000kr</p>
-            </div>
+            <div
+              className={this.state.active1 ? "shop-item-checkmark" : ""}
+              onClick={e => this.toggleClass("active1", e)}
+            />
+            {this.state.presentInfo1 ? (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo1", e)}
+                >
+                  >
+                </label>
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active1", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Pris: 1000kr</p>
+                  <div className="more-info-container" />
+                </div>
+              </div>
+            ) : (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo1", e)}
+                >
+                  >
+                </label>
+                <img
+                  className="shop-img"
+                  src="/images/elefant.png"
+                  alt="event"
+                  onClick={e => this.toggleClass("active1", e)}
+                />
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active1", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>
+                    Pris: 1000kr <div className="more-info-container" />
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
+
           <div
             className={this.state.active2 ? "shop-item-border" : "shop-item"}
             id="1"
-            onClick={e => this.toggleClass("active2", e)}
           >
             <div className={this.state.active2 ? "shop-item-overlay" : ""} />
-            <div className={this.state.active2 ? "shop-item-checkmark" : ""} />
-            <img className="shop-img" src="/images/hippo.png" alt="event" />
-            <div className="shop-info">
-              <p>Kay Bojesen Flodhäst Träleksak</p>
-              <p>Pris: 1000kr</p>
-            </div>
+            <div
+              className={this.state.active2 ? "shop-item-checkmark" : ""}
+              onClick={e => this.toggleClass("active2", e)}
+            />
+            {this.state.presentInfo2 ? (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo2", e)}
+                >
+                  >
+                </label>
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active2", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Pris: 1000kr</p>
+                  <div className="more-info-container" />
+                </div>
+              </div>
+            ) : (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo2", e)}
+                >
+                  >
+                </label>
+                <img
+                  className="shop-img"
+                  src="/images/hippo.png"
+                  alt="event"
+                  onClick={e => this.toggleClass("active2", e)}
+                />
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active2", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>
+                    Pris: 1000kr <div className="more-info-container" />
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
+
           <div
             className={this.state.active3 ? "shop-item-border" : "shop-item"}
             id="1"
-            onClick={e => this.toggleClass("active3", e)}
           >
             <div className={this.state.active3 ? "shop-item-overlay" : ""} />
-            <div className={this.state.active3 ? "shop-item-checkmark" : ""} />
-            <img className="shop-img" src="/images/zebra.png" alt="event" />
-            <div className="shop-info">
-              <p>Kay Bojesen Flodhäst Träleksak</p>
-              <p>Pris: 1000kr</p>
-            </div>
+            <div
+              className={this.state.active3 ? "shop-item-checkmark" : ""}
+              onClick={e => this.toggleClass("active3", e)}
+            />
+            {this.state.presentInfo3 ? (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo3", e)}
+                >
+                  >
+                </label>
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active3", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Pris: 1000kr</p>
+                  <div className="more-info-container" />
+                </div>
+              </div>
+            ) : (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo3", e)}
+                >
+                  >
+                </label>
+                <img
+                  className="shop-img"
+                  src="/images/zebra.png"
+                  alt="event"
+                  onClick={e => this.toggleClass("active3", e)}
+                />
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active3", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>
+                    Pris: 1000kr <div className="more-info-container" />
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
+
           <div
             className={this.state.active4 ? "shop-item-border" : "shop-item"}
             id="1"
-            onClick={e => this.toggleClass("active4", e)}
           >
             <div className={this.state.active4 ? "shop-item-overlay" : ""} />
-            <div className={this.state.active4 ? "shop-item-checkmark" : ""} />
-            <img className="shop-img" src="/images/elefant.png" alt="event" />
-            <div className="shop-info">
-              <p>Kay Bojesen Flodhäst Träleksak</p>
-              <p>Pris: 1000kr</p>
-            </div>
+            <div
+              className={this.state.active4 ? "shop-item-checkmark" : ""}
+              onClick={e => this.toggleClass("active4", e)}
+            />
+            {this.state.presentInfo4 ? (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo4", e)}
+                >
+                  >
+                </label>
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active4", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Pris: 1000kr</p>
+                  <div className="more-info-container" />
+                </div>
+              </div>
+            ) : (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo4", e)}
+                >
+                  >
+                </label>
+                <img
+                  className="shop-img"
+                  src="/images/elefant.png"
+                  alt="event"
+                  onClick={e => this.toggleClass("active4", e)}
+                />
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active4", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>
+                    Pris: 1000kr <div className="more-info-container" />
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
 
           <div
             className={this.state.active5 ? "shop-item-border" : "shop-item"}
             id="1"
-            onClick={e => this.toggleClass("active5", e)}
           >
             <div className={this.state.active5 ? "shop-item-overlay" : ""} />
-            <div className={this.state.active5 ? "shop-item-checkmark" : ""} />
-            <img className="shop-img" src="/images/hippo.png" alt="event" />
-            <div className="shop-info">
-              <p>Kay Bojesen Flodhäst Träleksak</p>
-              <p>Pris: 1000kr</p>
-            </div>
+            <div
+              className={this.state.active5 ? "shop-item-checkmark" : ""}
+              onClick={e => this.toggleClass("active5", e)}
+            />
+            {this.state.presentInfo5 ? (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo5", e)}
+                >
+                  >
+                </label>
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active5", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>Pris: 1000kr</p>
+                  <div className="more-info-container" />
+                </div>
+              </div>
+            ) : (
+              <div className="test-container">
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo5", e)}
+                >
+                  >
+                </label>
+                <img
+                  className="shop-img"
+                  src="/images/hippo.png"
+                  alt="event"
+                  onClick={e => this.toggleClass("active5", e)}
+                />
+                <div
+                  className="shop-info"
+                  onClick={e => this.toggleClass("active5", e)}
+                >
+                  <p>Kay Bojesen Flodhäst Träleksak</p>
+                  <p>
+                    Pris: 1000kr <div className="more-info-container" />
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
           <div
             className={this.state.active6 ? "shop-item-border" : "shop-item"}
@@ -115,9 +328,12 @@ class Web_shop extends React.Component {
               className={this.state.active6 ? "shop-item-checkmark" : ""}
               onClick={e => this.toggleClass("active6", e)}
             />
-            {this.state.presentInfo ? (
+            {this.state.presentInfo6 ? (
               <div className="test-container">
-                <label className="more-info-label" onClick={this.toggleInfo}>
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo6", e)}
+                >
                   >
                 </label>
                 <div
@@ -133,7 +349,10 @@ class Web_shop extends React.Component {
               </div>
             ) : (
               <div className="test-container">
-                <label className="more-info-label" onClick={this.toggleInfo}>
+                <label
+                  className="more-info-label"
+                  onClick={e => this.toggleInfo("presentInfo6", e)}
+                >
                   >
                 </label>
                 <img
