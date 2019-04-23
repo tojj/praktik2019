900428beca77120ae294bef8261e7292b6967d1f
Aleksandra <a.toholjevic@gmail.com>
2019-04-23 15:14:16 +0200

    structure

--- src/components/CreatePartyPage/Form/Form_footer/Checkout/index.js
+++ src/components/CreatePartyPage/Form/Form_footer/Checkout/index.js
@@ -5,31 +5,85 @@ import { Button, Input, FormGroup, Label } from 'reactstrap'
 class Checkout extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      loginOption: false,
+      noRegisterOption: false
+    }
 
+  }
+
+  loginToggle = () => {
+    this.setState({
+      loginOption: !this.state.loginOption
+    })
+  }
+
+  registerToggle = () => {
+    this.setState({
+      noRegisterOption: !this.state.noRegisterOption
+    })
   }
 
   render() {
     return (
-      <div className="box-container" id="checkout-container">
-        <div className="box text-left">
-          <h2 className="form-headline">Skapa konto</h2>
-          <FormGroup>
-            <Label for="email-input">E-post</Label>
-            <Input type="email" name="email" id="email-input" placeholder="E-post" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Pssword-input">Lösenord</Label>
-            <Input type="password" name="password" id="password-input" placeholder="Lösenord" />
-          </FormGroup>
-          <Button color="primary" type="button" >Registrera</Button>
-        </div >
-
-        <div className="box">
-          <h2 className="form-headline">Fortsätt utan konto</h2>
-          <Button color="primary" type="button" >Slutför</Button>
+      <div className="box-details-container details-container style-class" id="checkout-container">
+        <div className="box align-left">
+          <div className="form">
+            <h2 className="form-headline text-center">
+              Slutför
+              </h2>
+            <div className="input-group">
+              <input
+                className="radio-input"
+                id="radio3"
+                name="radio"
+                type="radio"
+                onClick={this.loginToggle}
+              />
+              <label className="radio-label" htmlFor="radio3">
+                Fortsätt som inloggad användare
+                </label>
+            </div>
+            <div className="input-group">
+              <input
+                className="radio-input"
+                id="radio4"
+                name="radio"
+                type="radio"
+                onClick={this.registerToggle}
+              />
+              <label className="radio-label" htmlFor="radio4">
+                Fortsätt utan inloggning
+                </label>
+            </div>
+          </div>
         </div>
-      </div >)
-
+        {this.state.loginOption ? (
+          <div>
+            <div className="login-container">
+              <div className="login-content">
+                <h2 className="form-headline">Skapa konto</h2>
+                <FormGroup>
+                  <Label for="email-input">E-post</Label>
+                  <Input type="email" name="email" id="email-input" placeholder="E-post" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="Pssword-input">Lösenord</Label>
+                  <Input type="password" name="password" id="password-input" placeholder="Lösenord" />
+                </FormGroup>
+                <Button color="primary" type="button" >Registrera</Button>
+              </div >
+            </div>
+          </div>) : null}
+        {this.state.noRegisterOption ? (<div className="box-container">
+          <div className="box align-left">
+            <div className="form">
+              <h2 className="form-headline">Fortsätt utan konto</h2>
+              <Button color="primary" type="button" >Slutför</Button>
+            </div>
+          </div>
+        </div>) : null}
+      </div>)
 
   }
 }
