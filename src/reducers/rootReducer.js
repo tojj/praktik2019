9e6db3144300501b8f3949621efa6b8ab0db098e
Pontus Nilsson <p0nta_@hotmail.com>
2019-04-26 10:24:40 +0200

    Removal of unessasary stuff on swish
    Also added redux to swish so it saves the amount onto the Prepp Page.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import BirthdayReducer from '../store/Birthday/BirthdayReducer'
+import SwishReducer from '../store/Swish/SwishReducer'
 
 export default combineReducers({
- birthday: BirthdayReducer
+ birthday: BirthdayReducer,
+ swish: SwishReducer
 })
