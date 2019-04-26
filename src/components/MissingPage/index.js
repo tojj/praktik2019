7e8d6070d80488e6a964246b4562d8ac4f633d76
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-04-26 11:13:31 +0200

    added {Component}

--- src/components/MissingPage/index.js
+++ src/components/MissingPage/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
 /**
  * Takes "link" & "linkName" as props to render extra navigation help.
@@ -13,11 +13,18 @@ class MissingPage extends Component {
   render() {
     return (
       <div className="mp-container">
-        <h1 className="mp-header">{this.props.title ? this.props.title : 'Sidan saknas'}</h1>
+        <h1 className="mp-header">
+          {this.props.title ? this.props.title : "Sidan saknas"}
+        </h1>
         <h4 className="mp-info">
           Vi kunde tyvärr inte hitta sidan {window.location.pathname}
         </h4>
-        <Link className="btn btn-outline-primary" to={this.props.link ? this.props.link : "/"}>{this.props.link ? this.props.linkName : 'Tillbaka till Start'}</Link>
+        <Link
+          className="btn btn-outline-primary"
+          to={this.props.link ? this.props.link : "/"}
+        >
+          {this.props.link ? this.props.linkName : "Tillbaka till Start"}
+        </Link>
         <img className="mp-background" src="/images/missingPageImg.png" />
       </div>
     )
