fb3d178b1d4287599c6a56d4ff423e8b5b24eeb1
Aleksandra <a.toholjevic@gmail.com>
2019-04-29 13:57:55 +0200

    importer

---
+++ importQna.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const supersecret = require('./supersecret')
+
+
+mongoose.connect(supersecret, { useNewUrlParser: true })
+global.db = mongoose.connection
+db.on("error", () => reject("Could not connect to DB"))
+db.once("open", () => {
+  console.log('Connected to DB')
+  importJsonDataToDb()
+})
+
+
+
+// Load Mongoose models
+let Qna = require('./models/Qna')
+
+// Load the json data from file
+let qnaData = require('./data/FAQ.json')
+
+async function importJsonDataToDb() {
+  let allQnaCount = await Qna.count()
+  // if the db already contains Fundraiser then delete them
+  if (allQnaCount > 0) {
+    console.log('Deleted old qnas', await Qna.remove({}))
+  }
+
+  // creates Fundraisers
+  for (let data of qnaData) {
+    let qna = new Qna(data)
+    await qna.save()
+  }
+  // after the import count the Fundraisers again
+  allQnasCount = await Qna.count()
+  console.log(`Imported ${allQnasCount} fundraisers to the database`)
+  // Exit the app
+  process.exit()
+}
