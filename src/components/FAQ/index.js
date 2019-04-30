9566a9210cbd00084d22260fbd1efa10ab3ea962
Jesper Asplund <jesper.asplund95@gmail.com>
2019-04-30 11:30:59 +0200

    Hot fix for FAQ, now only lists 5 qna's and has a button to load 5 more.

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -8,33 +8,46 @@ class FAQ extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      qnaContent: ''
+      qnaContent: '',
+      amount: 5,
+      totalAmount: ''
     }
+    this.getQnaAmountTotal()
     this.getAllQnaAndMount()
+    this.getQnaAmountTotal = this.getQnaAmountTotal.bind(this)
     this.getAllQnaAndMount = this.getAllQnaAndMount.bind(this)
     this.pushCounts = this.pushCounts.bind(this)
   }
-  async pushCounts(id) {    
-    let qna = await Qna.find(id)    
+  async pushCounts(id) {
+    let qna = await Qna.find(id)
     qna.counter++
     await qna.save()
   }
+  async getQnaAmountTotal() {
+    const allQnasAmount = await Qna.find()
+    this.setState({ totalAmount: allQnasAmount.length })
+  }
   async getAllQnaAndMount() {
-    const allQnaData = await Qna.find(`.find().sort({counter: -1}).exec()`)
+    const allQnaData = await Qna.find(`.find().limit(${this.state.amount}).sort({counter: -1}).exec()`)
     const allQna = allQnaData.map((qna, i) => {
       return (
-        <QuestionAndAnswer question={qna.question} answer={qna.answer} key={i} id={qna._id} count={qna.counter} clickHandler={this.pushCounts}/>
+        <QuestionAndAnswer question={qna.question} answer={qna.answer} key={i} id={qna._id} count={qna.counter} clickHandler={this.pushCounts} />
       )
     })
-
-    this.setState({qnaContent: allQna})
+    this.setState({
+      qnaContent: allQna,
+      amount: this.state.amount + 5
+    })
   }
-
+  
   render() {
     return (
       <div className="faq-wrapper p-5 bg-white">
         <h2>FAQ - Frågor och svar</h2>
         {this.state.qnaContent}
+        {this.state.qnaContent.length === this.state.totalAmount ? null :
+          <button type="button" className="mt-3 btn btn-outline-dark" onClick={this.getAllQnaAndMount}>Läs in fler</button>
+        }
       </div>
     )
   }
