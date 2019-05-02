d7cac401ac58312b1f094c356a7eb38f37766d73
Pontus Nilsson <p0nta_@hotmail.com>
2019-05-02 15:31:43 +0200

    added functionality and some styling (work in progress)

--- src/components/BirthdayInvite/index.js
+++ src/components/BirthdayInvite/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
 class BirthdayInvite extends React.Component {
-  
-    
+
   render() {
 
     return (
       <div className="invite-wrapper">
-        <input className="form-control invite-input"></input>
-        <button className="link-party-page send-button">Skicka</button>
+        <form className="email-form" onSubmit={this.props.addEmail}>
+          <input
+            className="target-invite-input"
+            placeholder="e-post"
+            ref={this.props.inputElement}
+            value={this.props.currentEmail.text}
+            onChange={this.props.handleInput}
+            type="email"
+          />
+          <button className="link-party-page add-email" type="submit">Lägg till</button>
+        </form>
       </div>
     )
   }
--- src/components/ConfirmationPage/index.js
+++ src/components/ConfirmationPage/index.js
@@ -1,13 +1,62 @@
 import React from 'react'
 import { Minus } from 'react-feather'
 import BirthdayInvite from '../BirthdayInvite'
+import BirthdayInviteList from '../BirthdayInviteList'
 
 class ConfirmationPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      emails: [],
+      currentEmail: { text: '', key: '' },
+      counter: 0,
+    }
+
+    setInterval(this.incrementCounter)
+
+  }
+
+  incrementCounter = () => {
+    this.setState(({counter}) => ({counter: counter++}))
+  }
+
+  handleInput = e => {
+    const emailText = e.target.value
+    const currentEmail =  {text: emailText, key: Date.now() }
+    this.setState({
+      currentEmail,
+    })
+  }
+
+  addEmail = e => {
+    e.preventDefault()
+    const newEmail = this.state.currentEmail
+    if (newEmail.text !== '') {
+      console.log(newEmail)
+      const emails = [...this.state.emails, newEmail]
+      this.setState({
+        emails: emails,
+        currentEmail: { text: '', key: '' },
+      })
+      console.log(emails)
+    }
+  }
+
+  deleteEmail = key => {
+    const filteredEmails = this.state.emails.filter(email => {
+      return email.key !== key
+    })
+    this.setState({
+      emails: filteredEmails
+    })
+  }
 
   redirectToYourParty = () => {
     let url = window.location.pathname.split("/")
     this.props.history.push("/kalas/" + url[2])
   }
+
+  
 
   render() {
 
@@ -16,10 +65,19 @@ class ConfirmationPage extends React.Component {
       <div>
         <div className="conf-wrapper bg-white">
           <h1 className="conf-headline">Grattis ditt kalas är skapat!</h1>
-          <p className="conf-info"><Minus />Nedanstående kan du bjuda in personer till kalaset, detta är givetvis valfritt. </p>
+          <p className="conf-info"><Minus className="delete-invite" />Nedanstående kan du bjuda in personer till kalaset, detta är givetvis valfritt. </p>
         </div>
         <div className="bday-invite-wrapper bg-white">
-          <BirthdayInvite />
+
+          <BirthdayInvite 
+            addEmail={this.addEmail}
+            inputElement={this.inputElement}
+            handleInput={this.handleInput}
+            currentEmail={this.state.currentEmail}
+            />
+          <BirthdayInviteList entries={this.state.emails} deleteEmail={this.deleteEmail} />
+
+          <button className="link-party-page send-button">Skicka</button>
           <button onClick={this.redirectToYourParty} className="link-party-page conf-button">Tryck här för att komma till kalaset!</button>
         </div>
       </div>
