149af33d1e7ffec280811a8c3794e64e0a1d77cd
Oliver Nilsson <oliver.nilsson94@gmail.com>
2019-05-06 15:11:16 +0200

    making things align

--- src/components/StartPage/EyeCatcher/index.js
+++ src/components/StartPage/EyeCatcher/index.js
@@ -1,15 +1,27 @@
 import React from "react"
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom"
 
-const EyeCatcher = (props) => (
+const EyeCatcher = props => (
   <div className="box-container eye-catcher" id="eye-catcher">
     <div className="box box-1">
-      <h2 className="main-heading" id="eye-catcher-heading">BÄSTA PRESENTEN</h2>
-      <p>Med Tojj kan alla få det kalas de förtjänar. Klicka på knappen nedan för att skapa ditt kalas nu!</p>
-      <Link to="/skapa-kalas" className="link-eye-catcher">Skapa kalas</Link>
+      <h2 className="main-heading" id="eye-catcher-heading">
+        BÄSTA PRESENTEN
+      </h2>
+      <p>
+        Med Tojj kan alla få det kalas de förtjänar. Klicka på knappen nedan för
+        att skapa ditt kalas nu!
+      </p>
+      <Link to="/skapa-kalas" className="link-eye-catcher">
+        Skapa kalas
+      </Link>
     </div>
-    <div className="box box-2">
-      <img className="eye-catcher-img box-img" id="small-eye-catcher" src="/images/eye-catcher-img.png" alt="Birthday card" />
+    <div className="box box-2 box-img-right">
+      <img
+        className="eye-catcher-img box-img"
+        id="small-eye-catcher"
+        src="/images/eye-catcher-img.png"
+        alt="Birthday card"
+      />
     </div>
   </div>
 )
