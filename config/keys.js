ba72135ab929b27ce839863ecaf01a3117dc3174
Jesper Asplund <jesper.asplund95@gmail.com>
2019-05-21 13:47:07 +0200

    keys

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,5 @@ dotenv.config();
 module.exports = {
    db_host: process.env.REACT_APP_DB_HOST,
    mail: process.env.REACT_APP_MAIL_SECRET,
-   port: process.env.SERVER_PORT
+   port: process.env.REACT_APP_SERVER_PORT
 };
