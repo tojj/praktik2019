0fa34190d61e03e0dd519bbbd79a516b19f1832f
Jesper Asplund <jesper.asplund95@gmail.com>
2019-06-26 09:07:37 +0200

    small code fix for iframe

--- src/components/StartPage/ProductInfo/index.js
+++ src/components/StartPage/ProductInfo/index.js
@@ -4,7 +4,14 @@ import staticData from "../../../staticData"
 const ProductInfo = () => (
   <div className="box-container prodinfo-container" id="prodinfo-container">
     <div className="box box-1 box-img-left">
-    <iframe title="Instructions video" width="100%" height="400px" src="https://www.youtube.com/embed/Mo6iovzWIA8" frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe
+        title="Instructions video"
+        width="100%"
+        height="400px"
+        src="https://www.youtube.com/embed/Mo6iovzWIA8"
+        frameBorder="0"
+        allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen />
     </div>
     <div className="box box-2">
       <h2 className="prodinfo-header">Kvalitet över kvantitet</h2>
