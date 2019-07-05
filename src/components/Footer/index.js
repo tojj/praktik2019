82b1507e4953408d8426b408bc231bd3b971ee22
Oliver <oliver.nilsson94@gmail.com>
2019-07-05 11:40:23 +0200

    removed old code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -117,31 +117,3 @@ const Footer = () => (
 )
 
 export default Footer
-
-{
-  /* <h1 className="footer-headliner">Tojj</h1>
-    <div className="links">
-      <div>
-        <p><a href="mailto:info@tojj.se" className="mail-link" >info@tojj.se</a></p>
-        <address>
-          <a
-            href="https://www.google.com/maps/place/Humlegatan+4,+211+27+Malm%C3%B6/@55.6071389,13.0082618,19.09z/data=!4m5!3m4!1s0x4653a3e350cd28af:0xb22bc0e96f471b67!8m2!3d55.6071248!4d13.0087734"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="adress-link">
-            Humlegatan 4, 211 27 Malmö
-    </a>
-        </address>
-      </div>
-      <div className="link-container">
-        <NavLink to={"/avtal"} activeStyle={{ fontWeight: "bold" }} className="avtal-link" > Avtal</NavLink>
-        <NavLink to={"/vanliga-fragor"} activeStyle={{ fontWeight: "bold" }} className="help-link">Hjälp</NavLink>
-      </div>
-    </div>
-    <div className="media-container">
-      <Facebook className="media-icon" size={36} />
-      <Twitter className="media-icon" size={36} />
-      <Linkedin className="media-icon" size={36} />
-
-    </div> */
-}
