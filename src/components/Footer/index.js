cfe0d92b17721947b79b578db42d930c31b26a27
Jesper Asplund <jesper.asplund95@gmail.com>
2019-07-08 11:48:31 +0200

    thanks mr gitkraken: footer merge FIX

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -65,33 +65,22 @@ const Footer = () => (
     <div className="footer-center">
       <div className="footer-inner">
         <label className="footer-help">Behöver du hjälp?</label>
-        <div className="footer-link">
-          <div>
-            <p>
-              
-              <a href="mailto:info@tojj.se" className="footer-link">
+        <div>
+          <p>
+
+            <a href="mailto:info@tojj.se" className="footer-link">
               <Mail size="18" /> info@tojj.se
               </a>
-            </p>
-            <address>
-              <a
-                href="https://www.google.com/maps/place/Humlegatan+4,+211+27+Malm%C3%B6/@55.6071389,13.0082618,19.09z/data=!4m5!3m4!1s0x4653a3e350cd28af:0xb22bc0e96f471b67!8m2!3d55.6071248!4d13.0087734"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="footer-link"
-              >
-               <MapPin size="18" /> Humlegatan 4, 211 27 Malmö
-              </a>
-            </address>
-          </div>
-          <div className="link-container">
-            <NavLink
-              to={"/vanliga-fragor"}
-              activeStyle={{ fontWeight: "bold" }}
+          </p>
+          <address>
+            <a
+              href="https://www.google.com/maps/place/Humlegatan+4,+211+27+Malm%C3%B6/@55.6071389,13.0082618,19.09z/data=!4m5!3m4!1s0x4653a3e350cd28af:0xb22bc0e96f471b67!8m2!3d55.6071248!4d13.0087734"
+              target="_blank"
+              rel="noopener noreferrer"
               className="footer-link"
             >
-              Humlegatan 4, 211 27 Malmö
-            </a>
+              <MapPin size="18" /> Humlegatan 4, 211 27 Malmö
+              </a>
           </address>
         </div>
         <div className="link-container">
