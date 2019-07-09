3be448669f64650ce4745f4359b2fe8860bf42ef
Oliver <oliver.nilsson94@gmail.com>
2019-07-09 09:16:46 +0200

    removed vid

--- src/components/StartPage/EyeCatcher/index.js
+++ src/components/StartPage/EyeCatcher/index.js
@@ -5,20 +5,6 @@ const videoURL = "/images/startpage/bg-vid.mp4"
 
 const EyeCatcher = () => (
   <div className="eye-catcher" id="eye-catcher">
-    <div className="video-container">
-      <video
-        className="bg-video"
-        id="background-video"
-        loop
-        autoPlay
-        muted={true}
-        preload="auto"
-        poster="/images/startpage/bg-img.jpg"
-      >
-        <source src={videoURL} type="video/mp4" />
-      </video>
-      <div className="bg-overlay" />
-    </div>
     <div className="box-1">
       <h2 className="main-heading" id="eye-catcher-heading">
         BÄSTA PRESENTEN
