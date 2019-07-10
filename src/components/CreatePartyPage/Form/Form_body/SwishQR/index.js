f8a16ff2905abe55d7e72ca7a9f58c79cea6bfb2
Jesper Asplund <jesper.asplund95@gmail.com>
2019-07-10 17:09:29 +0200

    info box test

--- src/components/CreatePartyPage/Form/Form_body/SwishQR/index.js
+++ src/components/CreatePartyPage/Form/Form_body/SwishQR/index.js
@@ -4,9 +4,8 @@ import {
   FormGroup,
   Label
 } from "reactstrap"
-import ToolTip from '../../../../ToolTip'
-
 import { connect } from 'react-redux'
+import ToolTip from '../../../../ToolTip'
 import { updateSwish } from '../../../../../store/Swish/SwishActions'
 
 class SwishQR extends React.Component {
@@ -20,30 +19,44 @@ class SwishQR extends React.Component {
   handleChange = e => {
     const val = e.target.value
     this.props.updateSwishAmount(val)
-    if (e.target.value < 50){
-      this.setState({error: true})
+    if (e.target.value < 50) {
+      this.setState({ error: true })
     } else {
-      this.setState({error: false})
+      this.setState({ error: false })
     }
   }
 
   render() {
     return (
       <div id="swish-container" className="swish-container">
-          <FormGroup className="swish-form">
-            <Label for="amount-input" className="birthday-label">Swishbelopp <ToolTip text="Välj en rimlig summa för varje gäst att swisha in." /></Label>
-            <Input
-              type="number"
-              name="swishMoney"
-              onChange={this.handleChange}
-              defaultValue={this.props.swishMoney ? this.props.swishMoney : ''}
-              placeholder="Belopp i SEK per person"
-              id="amount-input"
-              className=""
-              style={this.state.error ? {borderColor: 'red'} : {borderColor:'rgba(0,0,0,0.1)'}}
-            />
-          </FormGroup>
-      </div>
+        <FormGroup className="swish-form">
+          <Label for="amount-input" className="birthday-label">Swishbelopp <ToolTip text="Välj en rimlig summa för varje gäst att swisha in." /></Label>
+          <Input
+            type="number"
+            name="swishMoney"
+            onChange={this.handleChange}
+            defaultValue={this.props.swishMoney ? this.props.swishMoney : ''}
+            placeholder="Belopp i SEK per person"
+            id="amount-input"
+            className=""
+            style={this.state.error ? { borderColor: 'red' } : { borderColor: 'rgba(0,0,0,0.1)' }}
+          />
+        </FormGroup>
+        <p style={{
+          position: 'relative',
+          width: '100%',
+          fontWeight: '600',
+          fontSize: '1.2rem',
+          color: 'black',
+          fontFamily: 'Montserrat',
+          margin: '2rem auto',
+          display: 'block',
+          padding: '30px',
+          border: '5px dotted #3a8885'
+        }}>
+          Istället för att ta med egna presenter, går de inbjudna gästerna ihop för att tillsammans betala för presenten du har valt ovan.
+          </p>
+      </div >
     )
   }
 }
